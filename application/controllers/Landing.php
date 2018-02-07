7743758ac7314e58095a7da1609290b210f224a3
Relyce4 <relyce4@gmail.com>
2018-02-07 17:22:45 +0100

    Link resi assoluti
    Aggiornamento dei link con altri di tipo assoluto

--- application/controllers/Landing.php
+++ application/controllers/Landing.php
@@ -1,12 +1,12 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Landing extends CI_Controller
 {
 	public function __construct()
 	{
 		parent::__construct();
 		$this->load->model("Landing_model");
+		$this->load->helper('url');
+		$this->load->helper('url_helper');
 	}
 
 	public function index()
@@ -16,6 +16,13 @@ class Landing extends CI_Controller
 		$data["testate"] = $this->Landing_model->getTestate(6);
 		$this->load->view('header');
 		$this->load->view('landing', $data);
+		$this->load->view('footer');
+	}
+
+	public function view($uri = "login")
+	{
+		$this->load->view('header');
+		$this->load->view($uri);
 		$this->load->view('footer');
 	}
 }
--- application/views/header.php
+++ application/views/header.php
@@ -3,8 +3,8 @@
 
 <head>
     <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="./assets/img/logo.jpg">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?=base_url("/assets/img/apple-icon.png")?>">
+    <link rel="icon" type="image/png" href="<?=base_url("/assets/img/logo.jpg")?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>Postadvisor</title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
@@ -12,8 +12,8 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
     <!-- CSS Files -->
-    <link href="./assets/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="./assets/css/now-ui-kit.css?v=1.1.0" rel="stylesheet" />
+    <link href="<?=base_url("/assets/css/bootstrap.min.css")?>" rel="stylesheet" />
+    <link href="<?=base_url("/assets/css/now-ui-kit.css?v=1.1.0")?>" rel="stylesheet" />
 </head>
 
 <body class="index-page sidebar-collapse">
@@ -21,14 +21,14 @@
     <nav class="navbar navbar-expand-lg bg-success fixed-top navbar-transparent success " color-on-scroll="400">
         <div class="container">
             <div class="navbar-translate">
-					<img src="./assets/img/logo3.png" style="width: 200px;">
+					<img src="<?=base_url("/assets/img/logo3.png")?>" style="width: 200px;">
                 <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-bar bar1"></span>
                     <span class="navbar-toggler-bar bar2"></span>
                     <span class="navbar-toggler-bar bar3"></span>
                 </button>
             </div>
-            <div class="collapse navbar-collapse justify-content-end" id="navigation" data-nav-image="./assets/img/blurred-image-1.jpg">
+            <div class="collapse navbar-collapse justify-content-end" id="navigation" data-nav-image="<?=base_url("/assets/img/blurred-image-1.jpg")?>">
                 <ul class="navbar-nav">
                     <li class="nav-item">
                         <a class="nav-link" href="javascript:void(0)" onclick="scrollToDownload()">
@@ -37,21 +37,21 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="./documentation/tutorial-components.html" target="_blank">
+                        <a class="nav-link" href="#" target="_blank">
                             <i class="now-ui-icons files_paper"></i>
                             <p>Il nostro servizio</p>
                         </a>
                     </li>
-					 <li class="nav-item">
-                        <a class="nav-link" href="./documentation/tutorial-components.html" target="_blank">
-                            <i class="now-ui-icons users_single-02"></i>
-                            <p>I nostri clienti</p>
-                        </a>
-                    </li>
 					<li class="nav-item">
-                        <a class="nav-link" href="./documentation/tutorial-components.html" target="_blank">
+                        <a class="nav-link" href="#" target="_blank">
                             <i class="now-ui-icons ui-1_email-85"></i>
                             <p>Contattaci</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?=base_url("landing/view/login")?>" target="_blank">
+                            <i class="now-ui-icons users_circle-08"></i>
+                            <p>Accedi | Registrati</p>
                         </a>
                     </li>
                 </ul>
